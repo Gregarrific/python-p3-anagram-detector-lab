16c04fa79999c3a06dad71a4565f04594e78dcda
Gregory Dutton <greg.dutton@gmail.com>
2023-09-23 13:26:12 -0700

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+import ipdb
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.letter_list =  sorted(letter for letter in self.word)
+
+    def match(self, match_list):
+        matches = []
+        for word in match_list:
+            possible_anagram = sorted(letter for letter in word)
+            if (possible_anagram == self.letter_list):
+                matches.append(word)
+        return matches
+
+
+# ipdb.set_trace()
